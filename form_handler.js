64715ab3e1ded799aaf69f44a806b19ef9c07a9b
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-03-06 11:34:03 -0800

    began work on form encoding (UNTESTED)

--- form_handler.js
+++ form_handler.js
@@ -1,3 +1,23 @@
+function encodeForm() {
+    var str = document.getElementById("team_number").value + " " +
+        getCheckedId("drivetrain") + " " + getCheckedId("intake") + " " +
+        getCheckedId("indexing") + " " +
+        document.getElementById("autonumber").value + " " +
+        document.getElementById("autonotes").value + " " +
+        getCheckedId("climbabil") + " " +
+        document.getElementById("alliancenotes").value;
+    document.getElementById("demo").innerHTML = str;
+}
+
+function getCheckedId(radioName) {
+    var radio = document.getElementByName(radioName);
+    for (const ele of radio) {
+        if (ele.checked) {
+            return ele.id
+        }
+    }
+}
+
 function reset() {
     document.getElementById("entry_form").reset();
 }
